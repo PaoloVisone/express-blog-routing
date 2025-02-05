f3007546dc9d869432298ab7cf2c8a04afa42e91
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-05 17:58:45 +0100

    [fix] text

--- routers/posts.js
+++ routers/posts.js
@@ -5,32 +5,32 @@ const router = express.Router();
 // CRUD
 // index
 router.get('/', function (req, res) {
-    res.send('Lista di ricette');
+    res.send('Lista dei post');
 });
 
 // show
 router.get('/:id', function (req, res) {
-    res.send('Dettagli della ricetta' + req.params.id);
+    res.send('Dettagli del post' + req.params.id);
 });
 
 // store
 router.post('/', function (req, res) {
-    res.send('Creazione nuova ricetta');
+    res.send('Creazione nuovo post');
 });
 
 // update
 router.put('/:id', function (req, res) {
-    res.send('Modifica integrale della ricetta' + req.params.id);
+    res.send('Modifica integrale del post' + req.params.id);
 });
 
 // modify
 router.patch('/:id', function (req, res) {
-    res.send('Modifica parziale della ricetta' + req.params.id);
+    res.send('Modifica parziale del post' + req.params.id);
 });
 
 // destroy
 router.delete('/:id', function (req, res) {
-    res.send('Eliminazione della ricetta' + req.params.id);
+    res.send('Eliminazione del post' + req.params.id);
 });
 
 // ESPORTO IL ROUTER
